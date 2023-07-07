88bd96fffe6c99f583994679bec82822ef7a7a82
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-07-08 02:15:51 +0530

    added Providers for client-side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import Providers from '@/components/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,8 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers> {children}</Providers>
+        {/* Allow for more height on mobile devices*/}
+        <div className="h-40 md:hidden"></div>
       </body>
     </html>
   );
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,10 +1,15 @@
 'use client';
 import { ThemeProvider } from 'next-themes';
-
 import { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider></ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {/* Session Provider for client side authentication */}
+      <SessionProvider>{children}</SessionProvider>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
